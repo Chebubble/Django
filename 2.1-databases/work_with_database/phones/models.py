899d57d3abb6d4b32edce3ae7ccd75981623efcd
Dmitriy Cheblukov <cheba1256@gmail.com>
2023-05-12 18:01:40 +0300

    3 hw

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200, null=False)
+    price = models.PositiveIntegerField()
+    image = models.URLField(max_length=300)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=200, unique=True)
+
