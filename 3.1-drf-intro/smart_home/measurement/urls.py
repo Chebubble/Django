75c6db1f5039bec599fbe3a0af1e624e110f00da
Dmitriy Cheblukov <113828325+Chebubble@users.noreply.github.com>
2023-07-05 14:29:27 +0300

    5 HW

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from measurement.views import MeasurementViewSet, SensorDetailViewSet, SensorViewSet
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
-]
+    path('sensors/', SensorViewSet.as_view({'get': 'list'})),
+    path('sensors/', SensorViewSet.as_view({'post': 'create'})),
+    path('sensors/<pk>/', SensorDetailViewSet.as_view()),
+    path('measurements/', MeasurementViewSet.as_view({'post': 'create'})),
