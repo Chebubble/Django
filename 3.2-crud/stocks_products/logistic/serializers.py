581cab04ba4d85c0dc2df98b93e6d225cbf2ab7f
Dmitriy Cheblukov <113828325+Chebubble@users.noreply.github.com>
2023-10-26 22:03:19 +0300

    HW6

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -3,18 +3,33 @@ from rest_framework import serializers
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
+      class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = [
+            'product',
+            'quantity',
+            'price',
+        ]
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = [
+            'id',
+            'address',
+            'positions',
+        ]
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -22,6 +37,14 @@ class StockSerializer(serializers.ModelSerializer):
 
         # создаем склад по его параметрам
         stock = super().create(validated_data)
+        
+        for position in positions:
+            StockProduct.objects.create(
+                stock=stock,
+                product=position['product'],
+                quantity=position['quantity'],
+                price=position['price'],
+            )
 
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
@@ -35,6 +58,16 @@ class StockSerializer(serializers.ModelSerializer):
 
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
+
+        for position in positions:
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=position['product'],
+                defaults={
+                    'quantity': position['quantity'],
+                    'price': position['price'],
+                }
+            )
 
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
