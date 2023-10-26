cb4381fe6cc8a2cd7ec5a0994b473331a20c9b50
Dmitriy Cheblukov <113828325+Chebubble@users.noreply.github.com>
2023-10-26 22:04:45 +0300

    HW6

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -8,9 +8,12 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description',]
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filterset_fields = ['products',]
